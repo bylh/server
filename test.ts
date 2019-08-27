f0f06abd5980fd31bf62a23921ac9ce3f6d11f28
lihuan <lihuan@aiyunxiao.com>
2019-08-27 21:02:20 +0800

    temp

---
+++ test.ts
@@ -0,0 +1,52 @@
+let input = [{
+  id: '1',
+  name: '111'
+}, {
+  id: '2',
+  name: '222'
+}, {
+  id: '3',
+  name: '333'
+}];
+
+let output = [{
+  id: '4',
+  name: '444'
+}, {
+  id: '2',
+  name: '222'
+}, {
+  id: '3',
+  name: '333'
+}];
+
+let knows = [
+  [{
+    id: '1',
+    name: '111'
+  }, {
+    id: '2',
+    name: '222'
+  }],
+  [{
+    id: '1',
+    name: '111'
+  }, {
+    id: '3',
+    name: '333'
+  }],
+]
+// 找到删除的即input中存在，而output中不存在
+let deleteArr = input.filter(inputItem =>
+   output.findIndex(outputItem => outputItem.id === inputItem.id) === -1);
+console.log(deleteArr);
+
+// 找到增加的及output中存在而input中不存在的
+let addArr = output.filter(outputItem =>
+  input.findIndex(inputItem => inputItem.id === outputItem.id) === -1);
+console.log(addArr);
+
+knows.forEach(arr => {
+  // arr.splice()
+  arr.push(...addArr);
+})
