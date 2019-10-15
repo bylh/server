23036ed9ac013fbafda56c671529cd947fc1d329
li huan <li.huan@zhtu.net>
2019-10-16 00:48:05 +0800

    补充提交

--- src/photos/photos.service.ts
+++ src/photos/photos.service.ts
@@ -11,9 +11,7 @@ export class PhotosService {
     secret: Config.Unsplash.secret
   });
   async getPhotos(query = {query: ''}) {
-    if(query.query) {
-      
-    }
+    
     query.query = query.query ? encodeURI(query.query) : null;
     let data = await this.unsplash.photos.getRandomPhoto({
       ...query
