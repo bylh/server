15793b3882c1cfffc3ca505cc27d8cf408cbe65c
lihuan <494397353@qq.com>
2020-08-24 18:03:31 +0800

    add download module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,12 @@ import { AppService } from './app.service';
 import { NewsController } from './modules/news/news.controller';
 import { NewsService } from './modules/news/news.service';
 import { PhotosModule } from './modules/photos/photos.module';
+import { DownloadModule } from './modules/download/download.module';
+import { DownloadController } from './download/download.controller';
 
 @Module({
-  imports: [PhotosModule],
-  controllers: [AppController, NewsController],
+  imports: [PhotosModule, DownloadModule],
+  controllers: [AppController, NewsController, DownloadController],
   providers: [AppService, NewsService],
 })
 export class AppModule {}
