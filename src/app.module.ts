0cb2f9467e8d0c126f48ca28b642d160456a00a8
lihuan <494397353@qq.com>
2020-08-24 17:45:26 +0800

    remove yx module

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NewsController } from './modules/news/news.controller';
 import { NewsService } from './modules/news/news.service';
-import { YunxiaoModule } from './modules/yunxiao/yunxiao.module';
 import { PhotosModule } from './modules/photos/photos.module';
 
 @Module({
-  imports: [YunxiaoModule, PhotosModule],
+  imports: [PhotosModule],
   controllers: [AppController, NewsController],
   providers: [AppService, NewsService],
 })
