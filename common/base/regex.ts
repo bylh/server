d8c52d78190026047293e7f4a04d077fa6a885f8
lihuan <494397353@qq.com>
2020-08-24 17:39:29 +0800

    MOD：remove subtree common

--- common/base/regex.ts
+++
@@ -1,5 +0,0 @@
-// 参考 (https://github.com/ziishaned/learn-regex/blob/master/translations/README-cn.md)
-// 点运算符
-let dotReg = new RegExp(".ar", "g"); // g全局， i忽略大小写
-// dotReg = new RegExp(/.ar/g);
-console.log('元字符 .ar 测试', dotReg.test('target'));
